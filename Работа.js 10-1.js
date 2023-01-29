c0092e047ad957756aace3c3bbc32d19f6da749a
BerezutskiyIvan <123052736+BerezutskiyIvan@users.noreply.github.com>
2023-01-29 22:59:25 +0300

    Add files via upload

---
+++ Работа.js 10-1.js
@@ -0,0 +1,72 @@
+// 3) Задание
+// function reverseString(text) {
+//   return text.split("").reverse().join("");
+// }
+// const str = "This is another test";
+// let arr = str.split(" ");
+// for ([index, elem] of arr.entries()) {
+//   if (elem.length >= 5) {
+//     arr[index] = reverseString(elem);
+//   }
+// }
+
+// console.log(arr);
+
+
+// 4) Задание
+// function reverseStr(str) {
+//     return str.split("").reverse().join("");
+// }
+ 
+// alert(reverseStr("ЛЮБЛЮ КОЗОЧЕК"));
+
+
+// 5) Задание
+// function ucFirst(str) {
+//     if (!str) return str;
+
+//     return str[0].toUpperCase() + str.slice(1);
+//   }
+
+//   console.log( ucFirst("glhf") );
+
+// 7) Задание
+// function arrayNum(num) {
+//   let array = [];
+//   for (let i = 1; i <= num; i++) {
+//     array.push(i);
+//   }
+//   return array;
+// }
+
+// console.log(arrayNum(7));
+
+
+// 8) Задание
+// function arrayRandElement(arr) {
+//     var rand = Math.floor(Math.random() * arr.length);
+//     return arr[rand];
+// }
+
+// var arr = ['ЛИШРАК', 'ФУРИОН', 'КОТЛ', 'ТИНИ', 'МАМКИНКИНГ', 'СК', 'УРСА'];
+
+// console.log(arrayRandElement(arr));
+
+// 12) Задание
+// unction is_perfect(number) {
+// var temp = 0;
+//    for(var i=1;i<=number/2;i++) {
+//          if(number%i === 0) {
+//             temp += i;
+//           }
+//      }
+
+//      if(temp === number && temp !== 0) {
+//        console.log("Это гуд");
+//         }
+//       else {
+//        console.log("Это не гуд");
+//         }
+//  }
+// is_perfect(28);
+
